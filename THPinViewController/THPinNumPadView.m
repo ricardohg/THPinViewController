be705f3a418c9bf41b4c510b58e71cf75ca5e60a
Ricardo Hernandez <a.ricardohg@gmail.com>
2016-06-29 16:09:44 -0500

    compile fix

--- THPinViewController/THPinNumPadView.m
+++ THPinViewController/THPinNumPadView.m
@@ -81,8 +81,7 @@
             
             NSUInteger number = (row < 3) ? row * 3 + col + 1 : 0;
             THPinNumButton *button = [[THPinNumButton alloc] initWithNumber:number
-                                                                    letters:[self lettersForRow:row column:col]];
-            button.numbersFont = self.numbersFont;
+                                                                    letters:[self lettersForRow:row column:col] numbersFont:self.numbersFont];
             button.translatesAutoresizingMaskIntoConstraints = NO;
             button.backgroundColor = self.backgroundColor;
             [button addTarget:self action:@selector(numberButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
